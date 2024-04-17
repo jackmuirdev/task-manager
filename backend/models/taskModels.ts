5b5f7149211c494ce81b6bb8dcc943d81cb0259d
Jack Muir <jmuirbusiness@gmail.com>
2024-04-17 17:52:54 +0100

    Model schemas for tasks and users

---
+++ backend/models/taskModels.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['to do', 'in progress', 'completed'],
+    default: 'to do'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
---
+++ backend/models/userModels.ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value: any) {
+        // Password must contain at least 1 capital letter, 1 symbol, 1 number, and 1 letter
+        const passwordRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$/;
+        return passwordRegex.test(value);
+      },
+      message: "Password must contain at least 1 capital letter, 1 symbol, 1 number, and 1 letter"
+    }
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.matchPassword = async function (enteredPassword: any) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) { 
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
