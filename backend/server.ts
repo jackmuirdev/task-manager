c6c1855f65de144f5cc7ec87f4dced1cb0140d0e
Jack Muir <jmuirbusiness@gmail.com>
2024-04-17 17:48:51 +0100

    Middleware made for server

--- backend/server.ts
+++ backend/server.ts
@@ -5,24 +5,37 @@ import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./api/swaggerApis";
 import { connectDB } from './database/db'
+import { errorHandler, notFound } from './middleware/errorMiddlware';
 
 dotenv.config();
 
-const app = express();
+// Get the port from the environment variables
 const port = process.env.PORT;
+// Create an Express application
+const app = express();
 
+// Connect to the database
 connectDB();
 
+// Parse incoming request bodies in JSON format
 app.use(express.json());
+// Parse incoming request bodies in URL-encoded format
 app.use(express.urlencoded({ extended: true }));
+// Parse cookies attached to the client request
 app.use(cookieParser());
-app.use(cors())
+// Enable Cross-Origin Resource Sharing (CORS)
+app.use(cors());
 
+// Swagger API documentation
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 try {
   app.listen(port, () => {
